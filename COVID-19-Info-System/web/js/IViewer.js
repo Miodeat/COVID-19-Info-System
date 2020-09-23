b30de563c361b11e5fcb905d66cf7cb88c1190da
Miodeat <2522620804@qq.com>
2020-09-24 00:13:05 +0800

    create index page and finish the interface of viewer

---
+++ COVID-19-Info-System/web/js/IViewer.js
@@ -0,0 +1,13 @@
+IViewer = function () {
+
+};
+
+IViewer.prototype.drawStatistic = function (div, type,
+                                            width, height,
+                                            X, Y) {
+    console.log("Not support yet");
+};
+
+IViewer.prototype.drawMap = function (div, data) {
+    console.log("Not support yet");
+};
