5df302711afeb756fc8feaa087999ad8d6fa9209
Daisyjf <2637394747@qq.com>
2020-09-27 11:53:53 +0800

    疫情累计信息获获取

--- COVID-19-Info-System/src/servlets/getEpidemicInfoServlet.java
+++ COVID-19-Info-System/src/servlets/getEpidemicInfoServlet.java
@@ -1,6 +1,5 @@
-package sevlets;
+package servlets;
 
-import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import services.epidemicService;
@@ -12,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet(name = "getEpidemicInfoServlet")
 public class getEpidemicInfoServlet extends HttpServlet {
@@ -24,10 +25,10 @@ public class getEpidemicInfoServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         epidemic_Service = new epidemicService();
         JSONObject res = new JSONObject();
-        String sql = "select * from epidemic_data where update_time =(select MAX(update_time) from epidemic_data)";
+        String sql = "select * from epidemic_data where update_time =(select MAX(update_time) from epidemic_data) order by country";
         String dbName = "basicdb";
-        res = epidemic_Service.getEpidemicInfoService(sql,dbName);
-//        res = handleEpidemicData(res);
+        res.put("total",epidemic_Service.getEpidemicInfoService(sql,dbName));
+        res = getCountryTotal(res);
         out.write(res.toString());
     }
 
@@ -36,12 +37,65 @@ public class getEpidemicInfoServlet extends HttpServlet {
         doPost(request,response);
     }
 
-    JSONObject handleEpidemicData(JSONObject data){
+    JSONObject getCountryTotal(JSONObject data){
         JSONObject res = new JSONObject();
-        JSONArray array = res.getJSONArray("res");
+        JSONObject res2 = new JSONObject();
+        JSONObject res3 = new JSONObject();
+        JSONArray array = data.getJSONArray("res");
+        int all_confirmed = 0;
+        int all_death = 0;
+        int all_recovered = 0;
+        int country_confirmed = 0;
+        int country_death = 0;
+        int country_recovered = 0;
+        String last_country = "";
+        HashMap<String, List<Integer>> everyCountryData = new HashMap<String,List<Integer>>();
         for(int i = 0;i<array.size();i++){
+            JSONObject object = array.getJSONObject(i);
+            String country = object.get("country").toString();
+            int confirmed = Integer.parseInt(object.get("confirmed").toString());
+            int death = Integer.parseInt(object.get("death").toString());
+            int recovered = Integer.parseInt(object.get("recovered").toString());
+            all_confirmed += confirmed;
+            all_death += death;
+            all_recovered += recovered;
+            if(everyCountryData.containsKey(country)){
+                country_confirmed += confirmed;
+                country_death += death;
+                country_recovered += recovered;
+                if(i==array.size()-1){
+                    res3.put("confirmed",country_confirmed);
+                    res3.put("death",country_death);
+                    res3.put("recovered",country_recovered);
+                    res2.put(last_country,res3);
+                }
+            }else{
+                res3.put("confirmed",country_confirmed);
+                res3.put("death",country_death);
+                res3.put("recovered",country_recovered);
+                res2.put(last_country,res3);
+                country_confirmed = confirmed;
+                country_death = death;
+                country_recovered = recovered;
+                if(i==array.size()-1){
+                    res3.put("confirmed",country_confirmed);
+                    res3.put("death",country_death);
+                    res3.put("recovered",country_recovered);
+                    res2.put(country,res3);
+                }
+            }
+
+            last_country = country;
 
         }
+        res2.discard("");
+        res.put("everyCountryTotal",res2);
+        res2.clear();
+        res2.put("confirmed",all_confirmed);
+        res2.put("death",all_death);
+        res2.put("recovered",all_recovered);
+        res.put("allTotal",res2);
+        res.put("time",array.getJSONObject(0).getString("update_time"));
         return res;
     }
 }
