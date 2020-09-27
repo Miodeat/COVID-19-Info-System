453fbb67f59415c9326ca1b3ccf04f5969271c8c
Daisyjf <2637394747@qq.com>
2020-09-27 21:12:24 +0800

    修改数据库连接方式

---
+++ COVID-19-Info-System/src/servlets/getCollectiveRationalityInfoServlet.java
@@ -0,0 +1,37 @@
+package servlets;
+
+import net.sf.json.JSONObject;
+import services.collectiveRationalityService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "getCollectiveRationalityInfoServlet")
+public class getCollectiveRationalityInfoServlet extends HttpServlet {
+    private collectiveRationalityService collectiveRationality_Service ;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("content-type", "text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        collectiveRationality_Service = new collectiveRationalityService();
+        String Sql = "select country,update_time,rationality from collective_rationality ";
+        String dbName = "collectiverationaldb";
+        JSONObject res = collectiveRationality_Service.getCollectiveRationalityInfo(Sql,dbName);
+        System.out.println(res);
+        System.out.println("Path "+request.getSession().getServletContext().getRealPath(""));
+        out.write(res.toString());
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
